1b5e863be8a5a1af8032f13248e2f08eac4e93cd
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-09-01 19:55:09 +0700

    feat lesson18 in js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded',function(){
     'use strict';
+    //таймер
     function countTimer(deadline){
         let timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
-
+        let timeinterval;
+        
         function getZero(n){
             if(n<10){
                 n='0'+n;
@@ -22,9 +24,7 @@ window.addEventListener('DOMContentLoaded',function(){
                 return {timeRemaining,hours, minutes,seconds};
         }
         function updateClock(){
-            let timer = getTimeREmaining();
-            var timeinterval = setInterval(updateClock, 1000);
-           
+            let timer = getTimeREmaining();                    
             if (timer.timeRemaining < 0){
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
@@ -35,11 +35,65 @@ window.addEventListener('DOMContentLoaded',function(){
                 timerMinutes.textContent = getZero(timer.minutes);
                 timerSeconds.textContent = getZero(timer.seconds);
             }
+           
         }
-        
+        timeinterval=setInterval(updateClock, 1000);
         updateClock();
             
     }
-    countTimer('2 September 2020');
     
+    countTimer('2 September 2020');
+    //меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems=menu.querySelectorAll('ul>li');
+        const hendlerMenu =()=>{
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', hendlerMenu);
+        closeBtn.addEventListener('click', hendlerMenu);
+        for(let i=0; i<menuItems.length;i++){          
+            menuItems[i].addEventListener('click', hendlerMenu);
+        }
+
+    };
+    
+    toggleMenu();
+
+    const togglePopUp = () =>{
+        const popup=document.querySelector('.popup'),
+            popupContent = document.querySelector('.popup-content'),
+            popupBtn=document.querySelectorAll('.popup-btn'),
+            popupClose=document.querySelector('.popup-close');
+        let amimationInterval;
+        let persent = 0;
+        popupContent.style.top = 0;
+        function animation() { 
+            persent++;            
+            if(persent===50){
+                clearInterval(amimationInterval);                
+            }
+            popupContent.style.top=`${persent/5}%`;
+            console.log(popupContent.style.top);
+        }
+        
+        
+        popupBtn.forEach((elem)=>{
+            elem.addEventListener('click', ()=>{
+                popup.style.display='block';
+                amimationInterval = setInterval(animation, 10);
+                animation();
+            });
+        });
+        popupClose.addEventListener('click',()=>{
+            popup.style.display = 'none';
+            persent=0;
+        });
+       
+        
+    };
+    togglePopUp();
+
 });
