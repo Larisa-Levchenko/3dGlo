182e6c896d80ff52bbab47cb7f6e09efb2c2e7aa
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-09-11 20:35:54 +0700

    feat lesson26 in js

--- script/script.js
+++ script/script.js
@@ -47,7 +47,7 @@ window.addEventListener('DOMContentLoaded', function () {
         updateClock();
 
     }
-    countTimer('2 September 2020');
+    countTimer('12 September 2020');
 
     //меню
     const toggleMenu = () => {
@@ -63,10 +63,10 @@ window.addEventListener('DOMContentLoaded', function () {
 
         const getAnimation = () =>{
             if (top < blockTop) {
-                if ((top + 40) > blockTop) {
+                if ((top + 50) > blockTop) {
                     top = blockTop;
                 } else {
-                    top = top+40;
+                    top = top+50;
                     animation = window.requestAnimationFrame(getAnimation);
                 }                
             }           
@@ -360,6 +360,94 @@ window.addEventListener('DOMContentLoaded', function () {
         });
 
     };
-
     calc();
+    //sendForm
+    const sendForm = () =>{
+        const errorMessage='Что-то пошло не так...',
+            loadMessage ='Загрузка...',
+            successMessage ='Спасибо! Мы скоро свяжемся с Вами!';
+        
+        const forms = document.querySelectorAll('form'),
+            placeholderName = document.querySelectorAll('[placeholder="Ваше имя"]'),
+            placeholderText = document.querySelector('[placeholder="Ваше сообщение"]'),
+            placeholderPhone = document.querySelectorAll('[placeholder="Номер телефона"]');
+        console.log(placeholderPhone);
+        placeholderName.forEach((item)=>{
+            item.addEventListener('input', () => {                
+                item.value = item.value.replace(/[^^А-Яа-я ]/i, '');
+            });   
+        });
+
+        placeholderText.addEventListener('input', () => {
+            placeholderText.value = placeholderText.value.replace(/[^^А-Яа-я ]/i, '');
+        });
+
+        placeholderPhone.forEach((item) => {
+            item.addEventListener('input', () => {
+                item.value = item.value.replace(/[^0-9+]/i, '');              
+            });
+        });
+               
+        const postDate = (body,outputDate, errorData) =>{
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange',()=>{                
+                if(request.readyState!==4){
+                    return;
+                }
+                if(request.status===200){
+                    outputDate();                    
+                }else{
+                    errorData(request.status);
+                }
+            });
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type','multipart/form-data');
+            request.send(JSON.stringify(body));
+
+        };
+        const emptyStr = () =>{
+            let formMessage=document.querySelector('.create-message');            
+            formMessage.remove();
+        };
+
+        forms.forEach((form)=>{
+            form.addEventListener("submit", (event) => {
+                event.preventDefault();                
+      
+                const createMessage = document.createElement("div");
+                createMessage.textContent = loadMessage;
+                createMessage.classList.add('create-message');
+                createMessage.style.cssText = "font-size: 2rem";
+                createMessage.style.cssText = "color: #fff";
+                form.appendChild(createMessage);          
+      
+                const formData = new FormData(form);         
+      
+                for (let i = 0; i < form.length - 1; i++) {
+                  form[i].value = "";
+                }
+      
+                let body = {};
+                formData.forEach((value, key) => {
+                  body[key] = value;
+                });
+                postDate(
+                  body,
+                  () => {
+                    createMessage.textContent = successMessage;
+                  },
+                  (error) => {
+                    console.log(error);
+                    createMessage.textContent = errorMessage;
+                  }
+                );          
+                
+                setTimeout(emptyStr, 6000);
+                
+              });      
+          
+        });
+                
+    };
+    sendForm();
 });
